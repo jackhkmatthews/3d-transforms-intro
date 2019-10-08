c75a4f1afd055a9217de841bddd6b10e1565671b
Jack Matthews <jackhkmatthews@gmail.com>
2019-10-08 12:33:48 +0100

    feat(app): cleanup from create-react-app

--- src/components/hello-world/hello-world.stories.tsx
+++
@@ -1,5 +0,0 @@
-import { storiesOf } from "@storybook/react";
-import * as React from "react";
-import HelloWorld from "./hello-world.component";
-
-storiesOf("HelloWorld", module).add("default", () => <HelloWorld />);
